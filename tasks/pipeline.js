cf8de4b0d372030a11adbc4acbba4d4d6fcc9a4d
cherlas <vm@li.cm>
2017-12-17 14:43:34 +0800

    Locale support for months names on this page

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -10,7 +10,7 @@ var tranquilpeakJsFilesToInject = [
 var tranquilpeakCssFilesToInject = [
   'font-awesome.css',
   'jquery.fancybox.css',
-  'jquery.fancybox-thumbs.css',
+  'thumbs.css',
   'tranquilpeak.css'
 ];
 
