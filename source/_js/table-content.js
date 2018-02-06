df2917270bfefe42743c92e80f5e09e6cd86562c
cherlas <vm@li.cm>
2018-02-07 00:20:21 +0800

    add table content toggle

--- source/_js/table-content.js
+++ source/_js/table-content.js
@@ -24,6 +24,15 @@
     run: function() {
       var tocContent = '';
       var title = '<li>' + this.tocMessage + '</li>';
+      var icon = '<i class="fa fa-chevron-down">';
+      this.$ulToc.children('li').children('ul').each(function() {
+        $(this).parent('li').prepend(icon);
+        var self = this;
+        $(this).slideToggle(20);
+        $(this).parent('li').find('.fa').click(function() {
+          $(self).slideToggle(300);
+        });
+      });
       this.$ulToc.prepend(title);
       if (this.$tableContent.length) {
         tocContent = this.$ulToc[0];
