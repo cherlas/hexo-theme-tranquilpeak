39fb4bc05a63e4fbe0a5bb0e66322877d58c62a4
cherlas <vm@li.cm>
2018-02-07 23:39:30 +0800

    add table content toggle

--- source/_js/table-content.js
+++ source/_js/table-content.js
@@ -1,4 +1,4 @@
-/* eslint-disable brace-style */
+/* eslint-disable brace-style,max-len */
 (function($) {
   'use strict';
 
@@ -24,13 +24,27 @@
     run: function() {
       var tocContent = '';
       var title = '<li>' + this.tocMessage + '</li>';
-      var icon = '<i class="fa fa-chevron-down">';
-      this.$ulToc.children('li').children('ul').each(function() {
-        $(this).parent('li').prepend(icon);
-        var self = this;
-        $(this).slideToggle(20);
-        $(this).parent('li').find('.fa').click(function() {
-          $(self).slideToggle(300);
+      var icon = '<i class="fa fa-chevron-right">';
+      var selfOut = this;
+      this.$ulToc.find('ul').each(function() {
+        var liBlock = $(this).parent('li');
+        $(liBlock).prepend(icon);
+        console.log($(liBlock).css('padding-left'))
+        if ($(liBlock).css('padding-left') !== '20px') {
+          $(liBlock).css('padding-left', '20px');
+        }
+        var selfIn = this;
+        var count = 0;
+        selfOut.toggleContent(this, 20);
+        $(liBlock).find('.fa').click(function() {
+          selfOut.toggleContent(selfIn, 250);
+          if (count % 2 === 0) {
+            $(liBlock).find('i').removeClass('fa-chevron-right').addClass('fa-chevron-down');
+            count++;
+          } else {
+            $(liBlock).find('i').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+            count = 0;
+          }
         });
       });
       this.$ulToc.prepend(title);
@@ -46,6 +60,10 @@
         this.$ulToc.detach();
         this.$contentWrap.prepend(tocContent);
       }
+    },
+
+    toggleContent: function(element, time) {
+      $(element).slideToggle(time);
     }
   };
 
